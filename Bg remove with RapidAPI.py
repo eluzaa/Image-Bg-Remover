37c379be32eadec221f22322a43a1a7e56498c6b
Aditya Vikram Singh <116886399+eluzaa@users.noreply.github.com>
2024-09-06 11:17:22 +0530

    Update Bg remove with RapidAPI.py

--- Bg remove with RapidAPI.py
+++ Bg remove with RapidAPI.py
@@ -1,20 +1,28 @@
 import requests
 
+url = "https://background-remover.p.rapidapi.com/remove-background"
 
-url = 'https://api.remove.bg/v1.0/removebg'
-api_key = Enter Your API KEy
+image_file_path = "test.png"
+# Opening the uploaded file and here we used 'rb' mode as image is read as binary data
+with open(image_file_path, 'rb') as image_file:
+    files = {
+        'image': image_file
+    }
 
-response = requests.post(
-    url,
-    files={'image_file': open('test.jpg', 'rb')},
-    data={'size': 'auto'},
-    headers={'X-Api-Key': api_key},
-)
+    headers = {
+        "x-rapidapi-key": Your API Key,
+        "x-rapidapi-host": "background-remover.p.rapidapi.com"
+    }
 
+    response = requests.post(url, files=files, headers=headers)
+    print(f"Status Code: {response.status_code}")
 
-if response.status_code == requests.codes.ok:
-    # Basically here i am creating a new file (no-bg) and writing binary content in it...
-    with open('no-bg.png', 'wb') as out:
-        out.write(response.content)
-else:
-    print("Error:", response.status_code, response.text)
+    # Try to decode the response as JSON
+    try:
+        response_data = response.text
+        with open("link.json", 'a') as images:
+            images.write(response_data)
+    except requests.exceptions.JSONDecodeError:
+        # If it's not JSON, print the raw response
+        print("Response content is not valid JSON")
+        # print(response.text)
